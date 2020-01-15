565a2d36019964ddfc98086025a5627388afc2aa
Daniel Roth <drot@vzch.com>
2020-01-15 19:07:59 +0100

    Basic Arrangment list

--- src/app/arrangement/arrangement-list/arrangement-list.component.ts
+++ src/app/arrangement/arrangement-list/arrangement-list.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest } from 'rxjs';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { map } from 'rxjs/operators';
+import { ArrangementAPIActions } from '../actions';
+import { NgRedux } from '@angular-redux/store';
+import { AppState } from 'src/app/store/model';
+import { IArrangement } from '../model';
 
 @Component({
   templateUrl: './arrangement-list.component.html',
@@ -9,15 +13,29 @@ import { map } from 'rxjs/operators';
 })
 export class ArrangementListComponent implements OnInit {
   id$: Observable<string>;
+  arrangements$: Observable<IArrangement[]>;
 
   constructor(
+    private store: NgRedux<AppState>,
     private route: ActivatedRoute,
-
+    private arrangementActions: ArrangementAPIActions
   ) { }
 
   ngOnInit() {
     this.id$ = this.route.paramMap.pipe(
       map((params: ParamMap) =>
-        decodeURIComponent(params.get('id')))        
+        decodeURIComponent(params.get('id')))
     );
-  }}
+
+    this.store.dispatch(this.arrangementActions.load())
+    this.id$ = this.route.paramMap.pipe(
+      map((params: ParamMap) =>
+        decodeURIComponent(params.get('id')))
+    );
+
+    this.arrangements$ = combineLatest(this.id$, this.store.select(s => s.arrangements.items)).pipe(
+      map(i => {
+        return i[1].filter(r => r.parent.startsWith(i[0]));
+      }));
+  }
+}
--- src/app/arrangement/arrangement.module.ts
+++ src/app/arrangement/arrangement.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArrangementListComponent } from './arrangement-list/arrangement-list.component';
+import { MaterialModule } from '../material.module';
 
 
 
 @NgModule({
   declarations: [ArrangementListComponent],
   imports: [
+    MaterialModule,
     CommonModule
   ],
   exports: [ArrangementListComponent]
